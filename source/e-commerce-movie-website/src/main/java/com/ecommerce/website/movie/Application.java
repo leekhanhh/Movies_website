887fc16ced41352004cec4f7765e857027ae7b41
lyhongkhanh <lyhongkhanh101102@gmail.com>
2024-01-08 16:25:25 +0700

    initial project

---
+++ source/e-commerce-movie-website/src/main/java/com/ecommerce/website/movie/Application.java
@@ -0,0 +1,32 @@
+package com.ecommerce.website.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
+@SpringBootApplication
+//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
+@EnableScheduling
+@EnableAspectJAutoProxy
+public class Application {
+    @PostConstruct
+    public void init() {
+        // It will set Asia/Ho_Chi_Minh timezone
+        String timezone = "Asia/Ho_Chi_Minh";
+        TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+        // It will print Asia/Ho_Chi_Minh timezone
+        System.out.println("Spring boot application running in " + timezone + " timezone : " + new Date());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
