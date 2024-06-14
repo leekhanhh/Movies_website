2b9727c238185c80e1855d9004bb084c0866909c
lyhongkhanh <lyhongkhanh101102@gmail.com>
2024-06-14 09:29:13 +0700

    feat: list recently movies

--- source/e-commerce-movie-website/src/main/java/com/ecommerce/website/movie/controller/WatchedMovieController.java
+++ source/e-commerce-movie-website/src/main/java/com/ecommerce/website/movie/controller/WatchedMovieController.java
@@ -1,10 +1,10 @@
 package com.ecommerce.website.movie.controller;
 
-import com.ecommerce.website.movie.constant.Constant;
 import com.ecommerce.website.movie.dto.ApiResponseDto;
 import com.ecommerce.website.movie.dto.ResponseListDto;
-import com.ecommerce.website.movie.dto.movie.WatchedMovieDto;
+import com.ecommerce.website.movie.dto.movie.MovieDto;
 import com.ecommerce.website.movie.form.movie.watchedmovie.CreateWatchedMovieForm;
+import com.ecommerce.website.movie.mapper.MovieMapper;
 import com.ecommerce.website.movie.mapper.WatchedMovieMapper;
 import com.ecommerce.website.movie.model.Movie;
 import com.ecommerce.website.movie.model.WatchedMovies;
@@ -16,23 +16,16 @@ import org.bson.types.ObjectId;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
-
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -50,6 +43,8 @@ public class WatchedMovieController {
     MovieRepository movieRepository;
     @Autowired
     private MongoTemplate mongoTemplate;
+    @Autowired
+    MovieMapper movieMapper;
 //
 //    @PostMapping(value = "/create-watched-movie", produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
 //    @Transactional
@@ -119,5 +114,22 @@ public class WatchedMovieController {
         return apiResponseDto;
     }
 
-
+    @GetMapping(value = "/list-watched-movie-by-accountId", produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponseDto<ResponseListDto<MovieDto>> listWatchedMovieByAccountId(WatchedMovieCriteria watchedMovieCriteria, Pageable pageable){
+        ApiResponseDto<ResponseListDto<MovieDto>> apiResponseDto = new ApiResponseDto<>();
+        Query query = new Query(Criteria.where("accountId").is(watchedMovieCriteria.getAccountId()))
+                .with(Sort.by(Sort.Direction.DESC, "createdDate"))
+                .with(pageable);
+        List<WatchedMovies> watchedMoviesOnlyDtoList = mongoTemplate.find(query, WatchedMovies.class);
+        Page<WatchedMovies> watchedMoviePage = new PageImpl<>(watchedMoviesOnlyDtoList, pageable, watchedMoviesOnlyDtoList.size());
+        List<Movie> movieList = new ArrayList<>();
+        for (WatchedMovies watchedMovie : watchedMoviesOnlyDtoList) {
+            Optional<Movie> movie = movieRepository.findById(watchedMovie.getMovieId());
+            movie.ifPresent(movieList::add);
+        }
+        ResponseListDto<MovieDto> responseListDto = new ResponseListDto(movieMapper.toClientMovieDtoList(movieList), watchedMoviePage.getTotalElements(), watchedMoviePage.getTotalPages());
+        apiResponseDto.setResult(true);
+        apiResponseDto.setData(responseListDto);
+        return apiResponseDto;
+    }
 }
